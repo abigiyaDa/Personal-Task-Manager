00eecb19793ff3b6ee495e9c6739ec290392cd7f
Ruhama <ruhamaristu10@gmail.com>
2026-03-30 14:26:42 +0300

    Edit register page form

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -7,7 +7,6 @@ import { Link } from "react-router-dom";
 const Register = () => {
   const [form, setForm] = useState({
     fullName: "",
-    username: "",
     email: "",
     password: "",
     confirmPassword: "",
